9680e145e339a4a4206d4b9982a0522ea4976d56
etornam45 <navidben45@gmail.com>
2023-08-19 00:10:45 +0000

    Updated icons and cursor

--- url_shortner/src/App.tsx
+++ url_shortner/src/App.tsx
@@ -1,18 +1,19 @@
-import Footer from './components/Footer'
-import Header from './components/Header'
-import HomePage from './components/Content.Home'
-import './App.css'
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HomePage from "./components/Content.Home";
+import "./App.css";
+
 
 function App() {
   return (
     <>
-      <Header title="uShort"/>
+      <Header title="uShort" />
       <div className="body">
         <HomePage />
       </div>
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
